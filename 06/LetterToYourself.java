7f59921676aa1289f7d0bd0529629b8ea1c5a38b
Nathan-Wen <36411251+Nathan-Win-Wen@users.noreply.github.com>
2018-02-16 09:11:17 -0500

    Add files via upload

---
+++ 06/LetterToYourself.java
@@ -0,0 +1,18 @@
+public class LetterToYourself
+{
+	public static void main( String[] args) 
+	{
+		System.out.println("+------------------------------------------+"); 	
+		System.out.println("|                                      ####|"); 
+		System.out.println("|                                      ####|"); 		
+		System.out.println("|                                      ####|"); 		
+		System.out.println("|                                          |");
+		System.out.println("|                                          |");
+		System.out.println("|                       Nathan Wen         |");
+		System.out.println("|                       45 Wilson Drive    |");
+		System.out.println("|                       Donaby, 8K6 J6P    |");
+		System.out.println("|                                          |"); 	
+		System.out.println("+------------------------------------------+"); 
+	}
+}
+
---
+++ 07/Initials.java
@@ -0,0 +1,13 @@
+public class Initials
+{
+	public static void main( String[] args) 
+	{
+		System.out.println("N   N W   W");	
+		System.out.println("NN  N W   W");
+		System.out.println("N N N W   W");
+		System.out.println("N  NN W W W");
+		System.out.println("N   N WW WW");
+		System.out.println("N   N WW WW");
+		System.out.println("N   N W   W");
+	}
+}
