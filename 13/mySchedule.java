ecaa8db0fed66188ba5b8e5ac64771bb2ddc690e
Nathan-Wen <36411251+Nathan-Win-Wen@users.noreply.github.com>
2018-02-21 09:40:45 -0500

    Add files via upload

---
+++ 13/mySchedule.java
@@ -0,0 +1,21 @@
+public class mySchedule {
+    public static void main(String[] args) {
+        String course1 = "CompSci";
+        String course2 = "Math   ";
+        String course3 = "Science";
+        String course4 = "Tech   ";
+        String teacher1 = "Mr.Gallo";
+        String teacher2 = "Ms.B    ";
+        String teacher3 = "Ms.P    ";
+        String teacher4 = "Mr.K    ";
+        
+        
+        System.out.println("+-------------------------------------------+");
+        System.out.println("| 1 |                " + course1 + " | " + teacher1 + " |");
+        System.out.println("| 2 |                " + course2 + " | " + teacher2 + " |");
+        System.out.println("| 3 |                " + course3 + " | " + teacher3 + " |");
+        System.out.println("| 4 |                " + course4 + " | " + teacher4 + " |");
+        System.out.println("+-------------------------------------------+");
+    }
+}
+        
